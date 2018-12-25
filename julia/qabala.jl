c7d969daf0b05177eda8e799d3d40cb4fa390bc2
sazare <song.of.sand@gmail.com>
2018-12-25 19:57:50 +0900

    major changes qabala.jl 	modified:   qabala.jl 	modified:   test_qabala.jl

--- julia/qabala.jl
+++ julia/qabala.jl
@@ -1,12 +1,26 @@
 # qabala -- an framework for Quantom Computation
 # 
 
+#### utility
+nyi() = println("not yet implemented")
+
+#### elements
+struct Qubit
+	state  :: Array{Complex{Float64}}
+	obsp   :: Bool
+end
+
+makeQubit(c) = Qubit(c, false)
+makeObserved(c) = Qubit(c, true)
+equalq(q1, q2) = (q1.state == q2.state)
+equalqa(q1, q2) = all(map((x,y)->x≈y,q1.state,q2.state))
+
 ##### constants
 #
 ## States
-zero1 = [1+0im, 0+0im]
-one1  = [0+0im, 1+0im]
-Q(φ) = [1 0; 0 ℯ^(φ*im)]
+zero1 = makeQubit([1.0+0.0im, 0.0+0.0im])
+one1  = makeQubit([0.0+0.0im, 1.0+0.0im])
+Q(φ) = makeQubit([1.0 0.0; 0.0 ℯ^(φ*im)])
 
 ## GATE
 X=[0 1;1 0]
@@ -17,8 +31,7 @@ H = 1/sqrt(2)*[1 1;1 -1]
 
 
 ###### functions
-#
-
+# primitives on a matrix of complex
 isunitary(x) = (x ≈ x')
 isprovability(x) = (sum(abs2.(x)) ≈ 1.0)
 
@@ -35,7 +48,7 @@ end
 function randumqubit()
   θ = rand(0:0.01:π/2)
   φ = rand(0:0.01:π)
-  cos(θ/2)*ket(0/2) + ℯ^(im*φ)*sin(θ/2)*ket(1)
+  makeQubit(cos(θ/2)*ket(0/2) + ℯ^(im*φ)*sin(θ/2)*ket(1))
 end
 
 function randumqubit(n::Number)
@@ -44,17 +57,24 @@ end
 
 
 function qbits(α, β)
-  [α, β]
+	makeQubit([α, β])
 end
 
-
-function gate(op, ψ)
-  op*ψ 
+function gate(op, q)
+	makeQubit(op*(q.state))
 end
 
-function observe(ψ, e)
-	p = (e * e')*ψ
-	p' * p
+function observe(q::Qubit, e::Qubit)
+	s = q.state
+	es = e.state
+	p = (e * e')*s
+	makeObserved(p' * p)
 end
 
+function measure(q::Qubit, e::Qubit)
+	nyi()
+	if q.obsp return q end
+	pe = observe(q, e)
+        if rand() >= pe; return e end
+end
 
--- julia/test_qabala.jl
+++ julia/test_qabala.jl
@@ -3,13 +3,18 @@ using Test
 include("qabala.jl")
 
 @testset "gate X" begin
- @test one1 == gate(X, zero1)
- @test zero1 == gate(X, one1)
+	@test equalq(one1 , gate(X, zero1))
+	@test equalq(zero1 , gate(X, one1))
 end
 
 @testset "gate H" begin
- @test zero1 ≈ gate(H, gate(H, zero1))
+	@test equalqa(zero1, gate(H, gate(H, zero1)))
 
+end
+
+@testset "obserbation in progress" begin
+ rr = randumqubit()
+ @test equalqa(zero1, observe(rr, zero1))
 end
 
 
